6b83e555ca2646c1091acbad934060704e9b0f5f
Keri Marie <kerimarie@Keringtons.local>
2017-05-05 14:37:24 -0500

    Initial commit for COTF website

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Laravel</title>
+
+        <link rel="stylesheet" type="text/css" href="/css/master.css">
+
+        <link href="https://fonts.googleapis.com/css?family=Fjalla+One|Oswald|Sacramento" rel="stylesheet">
+
+    </head>
+    <body>
+        <div class="container">
+            <div class="content">
+            <br><br><br>
+                <img src="../img/logo.png" class="logo">
+                <br><br>
+                <h1>coming soon...</h1>
+            </div>
+        </div>
+    </body>
+</html>
