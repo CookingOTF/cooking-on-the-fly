3164f3d5ea364b5f816bc8c58789817cbee09e63
Jacob Monnikendam <jacobmonn@sbcglobal.net>
2017-05-06 21:01:25 -0500

    adds (currently disabled) authentication to dashboard route

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -24,3 +24,11 @@ if (isset($_SERVER['REQUEST_URI']) and substr($_SERVER['REQUEST_URI'], -1) !== '
 }
 
 Route::get('/', 'MainController@welcome');
+
+Route::get('/dashboard/', /*['middleware' => 'auth']*/, 'MainController@dashboard');
+
+Route::get('/login/', 'Auth\AuthController@getLogin');
+Route::post('/login/', 'Auth\AuthController@postLogin');
+
+Route::get('/signup/', 'Auth\AuthController@getRegister');
+Route::post('/signup/', 'Auth\AuthController@postRegister');
