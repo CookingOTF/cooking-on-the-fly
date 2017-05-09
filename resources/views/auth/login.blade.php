e0d8802fad3d971450340fa178cd70082fd1133f
Keri Marie <kerimarie@Keringtons.local>
2017-05-09 10:52:43 -0500

    Updating CSS and HTML for login, footer, nav, welcome page. Also adding photos for internal pages.

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -5,23 +5,25 @@
 @section('content')
 
 
+<br><br><br>
 <section class="sign_in_container">
-	<h1>Sign In</h1>
+	<h2 class="sign_in_heading">SIGN IN</h2>
 	<div class="sign_in">
 		<div class="sign_in_content">
 			<form action="#" method="post">
-				<label for="email">Your email</label>
+				<label for="email" id="email">Your email</label>
 					<br><input type="email" name="email" id="email" class="inpt" required="required" placeholder="Your email">
 				<br><br>
-				<label for="password">Your password</label>
+				<label for="password" id="password">Your password</label>
 					<br><input type="password" name="password" id="password" class="inpt" required="required" placeholder="Your password">
 				
 				<br>
 				<input type="checkbox" id="remember" class="checkbox" checked>
 						<label for="remember" id="remember_me">Remember me</label><br>
-				<br><br>
+				<br>
 				<div class="submit-area">
-					<input type="submit" value="Sign in" class="submit"><br>
+					<input type="submit" value="SIGN IN" class="submit">
+				<br><br>
 				<a href="#" class="more" id="forgot_password">Forgot your password?</a><br><br><br>
 				</div>
 			</form>
--- resources/views/partials/nav.blade.php
+++ resources/views/partials/nav.blade.php
@@ -1,11 +1,11 @@
 <nav>
   <div class="nav">
     <ul>
-      <li id="home" class="home"><a href="/">HOME</a></li>
-      <li id="about" class="about"><a href="/login/">LOG IN</a></li>
-      <li class="logo"><a href="/"><img src="../img/logo.png" class="nav_logo"></a></li>
-      <li id="news" class="news"><a href="/signup/">SIGN UP</a></li>
-      <li id="contact" class="contact"><a href="/browse/">BROWSE</a></li>
+      <li id="home" class="home"><a href="/">HOME</a></li>&nbsp;
+      <li id="signin" class="signin"><a href="/login/">SIGN IN</a></li>&nbsp;
+      <li class="nav_logo"><a href="/"><img src="../img/logo.png" class="nav_logo"></a></li>&nbsp;
+      <li id="signup" class="signup"><a href="/signup/">SIGN UP</a></li>&nbsp;
+      <li id="browse" class="browse"><a href="/browse/">BROWSE</a></li>
     </ul>
   </div>
 </nav>
