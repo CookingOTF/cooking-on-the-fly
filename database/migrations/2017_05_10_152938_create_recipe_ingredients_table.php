d49445c0ff2c4013c89e95d5636eb97537f90f1e
Jacob Monnikendam <jacobmonn@sbcglobal.net>
2017-05-10 10:35:23 -0500

    creates recipe_ingredients junction table

---
+++ database/migrations/2017_05_10_152938_create_recipe_ingredients_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateRecipeIngredientsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('recipe_ingredients', function (Blueprint $table) {
+            $table->integer('recipe_id')->unsigned()->references('recipes')->on('id');
+            $table->integer('ingredient_id')->unsigned()->references('ingredients')->on('id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('recipe_ingredients');
+    }
+}
