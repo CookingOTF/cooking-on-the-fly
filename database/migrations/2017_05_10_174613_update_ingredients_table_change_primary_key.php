c85bfb91fa1653930816aa54a77bc200db08a293
Jacob Monnikendam <jacobmonn@sbcglobal.net>
2017-05-10 13:18:33 -0500

    Updates ingredients and recipe_ingredients tablesd primary key of ingredients is now "name" adds nullable "display_name" column to ingredients which can be shown to users in places like their fridge renames "display_name" column on recipe_ingredients to "display_in_recipe"

---
+++ database/migrations/2017_05_10_174613_update_ingredients_table_change_primary_key.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class UpdateIngredientsTableChangePrimaryKey extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('ingredients', function($table) {
+            $table->dropColumn('id');
+            $table->primary('name');
+            $table->string('display_name')->nullable();
+        });
+        Schema::table('recipe_ingredients', function($table) {
+            $table->dropColumn('ingredient_id');
+            $table->string('ingredient_name')->references('name')->on('ingredients');
+            $table->renameColumn('display_name', 'display_in_recipe');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('ingredients', function($table) {
+            $table->dropPrimary(['name']);
+            $table->dropColumn('display_name');
+        });
+        Schema::table('ingredients', function($table) {
+            $table->increments('id');
+        });
+    }
+}
