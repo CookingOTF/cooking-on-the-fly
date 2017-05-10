185ebd2eca703f32efa14a98fdb7e2104f1492fc
Jacob Monnikendam <jacobmonn@sbcglobal.net>
2017-05-10 13:55:29 -0500

    fixes filepaths again

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers\Auth;
 
-use App\Models\User;
+use App\User;
 use Validator;
 use App\Http\Controllers\BaseController;
 use Illuminate\Foundation\Auth\ThrottlesLogins;
