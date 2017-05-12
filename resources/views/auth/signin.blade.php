4623b668655b248fc56b634440b1a1eda8476dac
Keri Marie <kerimarie@Keringtons.local>
2017-05-12 16:13:23 -0500

    Updating sign in and sign up pages to show opaque background photo. Updated CSS file as well (for the same reasons-ish)

--- resources/views/auth/signin.blade.php
+++ resources/views/auth/signin.blade.php
@@ -3,12 +3,25 @@
 @include('partials.nav')
 @stop
 
+
 @section('custom_css')
 	<style type="text/css">
 		body {
+			position: relative;
+		}
+
+		body:after{
+			content : "";
+		    display: block;
+		    position: absolute;
+		    top: 0;
+		    left: 0;
 			background-image: url('/img/pasta.png');
-			background-size: 100%;
-			opacity: 0.5;
+			background-size: cover;
+			width: 100%;
+    		height: 100%;
+			opacity: 0.4;
+			z-index: -1;
 		}
 	</style>
 @stop
@@ -39,7 +52,7 @@
 				<br>
 
 				<div class="sign_in_remember_me">
-					<input type="checkbox" id="signin_remember" class="checkbox" checked>
+					<input type="checkbox" id="signin_checkbox" class="checkbox" checked>
 						<label for="remember" id="signin_remember_me">Remember me</label>
 				</div>
 				<br><br>
--- resources/views/auth/signup.blade.php
+++ resources/views/auth/signup.blade.php
@@ -6,8 +6,20 @@
 @section('custom_css')
 	<style type="text/css">
 		body {
+			position: relative;
+		}
+
+		body:after{
+			content : "";
+		    display: block;
+		    position: absolute;
+		    top: 0;
+		    left: 0;
 			background-image: url('/img/spaghetti.png');
-			background-size: cover;
+			width: 100%;
+    		height: 100%;
+			opacity: 0.4;
+			z-index: -1;
 		}
 	</style>
 @stop
