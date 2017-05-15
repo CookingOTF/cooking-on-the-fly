c010a30fc8d7a8d93b4ba11a94dae568eb02b1a0
Jacob Monnikendam <jacobmonn@sbcglobal.net>
2017-05-15 11:01:49 -0500

    adds redirectPath and loginPath properties

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -14,6 +14,10 @@ class AuthController extends BaseController
 
     protected $registerPage = 'auth.signup';
 
+    protected $redirectPath = '/dashboard/';
+
+    protected $loginPath = '/signin/';
+
     /*
     |--------------------------------------------------------------------------
     | Registration & Login Controller
