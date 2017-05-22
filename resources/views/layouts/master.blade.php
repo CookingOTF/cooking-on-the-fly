46d347c2085574f2f114ee80b94cea6e5be30c1c
Jacob Monnikendam <jacobmonn@sbcglobal.net>
2017-05-22 09:27:25 -0500

    moves scripts to bottom of body

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -12,13 +12,8 @@
 		<!-- Latest compiled and minified CSS -->
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 
-		<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-    	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-
   		{{-- CUSTOM CSS --}}
 		<link rel="stylesheet" type="text/css" href="/css/master.css">
-		{{-- CUSTOM JS --}}
-		<script type="text/javascript" src="/js/master.js"></script>
 
 		<link href="https://fonts.googleapis.com/css?family=BenchNine:300|Sofia" rel="stylesheet">
  
@@ -26,8 +21,6 @@
 
 </head>
 <body>
-	<!-- Latest compiled and minified JavaScript -->
-	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
 	@yield('navbar')
 	{{-- @include('partials.header') --}}
 
@@ -40,5 +33,20 @@
 			@include('partials.footer')
 		</p>
 	</div>
+	{{-- jQuery --}}
+	<script
+		src="https://code.jquery.com/jquery-3.2.1.min.js"
+		integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4="
+		crossorigin="anonymous">
+	</script>
+	{{-- Bootstrap JS --}}
+	<script
+		src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"
+		integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa"
+		crossorigin="anonymous">
+	</script>
+	{{-- Custom JS (master) --}}
+	<script type="text/javascript" src="/js/master.js"></script>
+	@yield('script')
 </body>
 </html>
